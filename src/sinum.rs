6b08fe256c6b74b85357b93f99179e936e38230a
Victor <victor@caern.de>
2024-01-06 22:22:01 +0100

    Improved documentation crate API.

--- src/sinum.rs
+++ src/sinum.rs
@@ -60,6 +60,13 @@ impl Prefix {
 	pub const MIN_EXP: i8 = -15;
 
 	/// Return the factor represented by this prefix.
+	///
+	/// # Example
+	/// ```
+	/// # use sinum::Prefix;
+	/// assert_eq!( Prefix::Peta.as_f64(), 1e15f64 );
+	/// assert_eq!( Prefix::Femto.as_f64(), 1e-15f64 );
+	/// ```
 	pub fn as_f64( &self ) -> f64 {
 		match self {
 			Self::Femto => 1e-15,
@@ -79,6 +86,13 @@ impl Prefix {
 	}
 
 	/// Returns the exponent representing this prefix.
+	///
+	/// # Example
+	/// ```
+	/// # use sinum::Prefix;
+	/// assert_eq!( Prefix::Peta.exp(), 15i8 );
+	/// assert_eq!( Prefix::Femto.exp(), -15i8 );
+	/// ```
 	pub fn exp( &self ) -> i8 {
 		match self {
 			Self::Femto =>  -15,
@@ -101,7 +115,19 @@ impl Prefix {
 impl TryFrom<i8> for Prefix {
 	type Error = PrefixError;
 
-	/// Returns a `Prefix` from the exponent `item`.
+	/// Returns a `Prefix` with the exponent of `item`.
+	///
+	/// If there is no Prefix representing this exponent a `PrefixError` will be returned.
+	///
+	/// # Example
+	/// ```
+	/// # use sinum::Prefix;
+	/// assert_eq!( Prefix::try_from( -3 ).unwrap(), Prefix::Milli );
+	/// assert_eq!( Prefix::try_from( -2 ).unwrap(), Prefix::Centi );
+	/// assert_eq!( Prefix::try_from( 0 ).unwrap(), Prefix::Nothing );
+	/// assert_eq!( Prefix::try_from( 3 ).unwrap(), Prefix::Kilo );
+	/// assert_eq!( Prefix::try_from( 15 ).unwrap(), Prefix::Peta );
+	/// ```
 	fn try_from( item: i8 ) -> Result<Self, Self::Error> {
 		let res = match item {
 			-15 => Self::Femto,
@@ -160,6 +186,13 @@ pub struct SiNum {
 
 impl SiNum {
 	/// Create a new `SiNum` representing the numeric value `num` without any prefix.
+	///
+	/// # Example
+	/// ```
+	/// # use sinum::SiNum;
+	/// assert_eq!( SiNum::new( 9999.9 ).as_f64(), 9999.9 );
+	/// assert_eq!( SiNum::new( 99999.9 ).as_f64(), 99999.9 );
+	/// ```
 	pub fn new( num: f64 ) -> Self {
 		Self {
 			mantissa: num,
@@ -167,7 +200,16 @@ impl SiNum {
 		}
 	}
 
-	/// Creates a new `SiNum` from `self` and applying `prefix`. This will change the numeric value of the number from `self` since the mantissa is staying the same while the prefix is modified.
+	/// Creates a new `SiNum` from `self` and applying `prefix`.
+	///
+	/// *Note:* The numeric value of the new `SiNum` will be different from `self` (aside from using the same `Prefix`) since the mantissa is staying the same while the `Prefix` is modified.
+	///
+	/// # Example
+	/// ```
+	/// # use sinum::{SiNum, Prefix};
+	/// assert_eq!( SiNum::new( 9999.9 ).with_prefix( Prefix::Mega ).as_f64(), 9_999_900_000.0 );
+	/// assert_eq!( SiNum::new( 9999.9 ).with_prefix( Prefix::Milli ).as_f64(), 9.9999 );
+	/// ```
 	pub fn with_prefix( self, prefix: Prefix ) -> Self {
 		Self {
 			mantissa: self.mantissa,
@@ -175,7 +217,22 @@ impl SiNum {
 		}
 	}
 
-	/// Creates a new `SiNum` from `self` at the specified `prefix`. This will *not* change the numeric value of the number from `self` since the mantissa is being modified alongside the prefix to reflect the same numeric value as before.
+	/// Creates a new `SiNum` from `self` at the specified `prefix`.
+	///
+	/// The numeric value of the new `SiNum` will be identical to `self` (apart from possible floating point rounding errors) since the mantissa is being modified alongside the prefix to reflect the same numeric value as before.
+	///
+	/// # Example
+	/// ```
+	/// # use sinum::{SiNum, Prefix};
+	/// let num = SiNum::new( 9999.9 );
+	///
+	/// assert_eq!( num.as_f64(), 9_999.9 );
+	/// assert_eq!( num.to_prefix( Prefix::Milli ).as_f64(), 9999.9 );
+	/// assert_eq!( num.to_prefix( Prefix::Milli ).mantissa(), 9_999_900.0 );
+	///
+	/// assert_eq!( num.to_prefix( Prefix::Kilo ).as_f64(), 9999.9 );
+	/// assert_eq!( num.to_prefix( Prefix::Kilo ).mantissa(), 9.9999 );
+	/// ```
 	pub fn to_prefix( self, prefix: Prefix ) -> Self {
 		let factor = self.prefix.as_f64() / prefix.as_f64();
 		Self {
@@ -184,10 +241,30 @@ impl SiNum {
 		}
 	}
 
-	/// Creates a new `SiNum` from `self` with less than 4 digits in front of the decimal point or less than 3 zeros after the decimal point if the digit in front of the decimal point is also 0.
+	/// Creates a new `SiNum` from `self` with a reduced numbers of digits of the mantissa (see `mantissa()`) required to represent the number:
 	///
-	/// * 1234 → 1.234 k
-	/// * 0.001 → 1.0 m
+	/// * No more than 3 digits in front of the decimal point.
+	/// 	(1234 → 1.234 k)
+	///
+	/// * No zero in front of the decimal point.
+	/// 	(0.001 → 1.0 m)
+	///
+	/// # Example
+	/// ```
+	/// # use sinum::{SiNum, Prefix};
+	/// assert_eq!(
+	/// 	SiNum::new( 1000.0 ).shorten().unwrap(),
+	/// 	SiNum::new( 1.0 ).with_prefix( Prefix::Kilo )
+	/// );
+	/// assert_eq!(
+	/// 	SiNum::new( 0.001 ).shorten().unwrap(),
+	/// 	SiNum::new( 1.0 ).with_prefix( Prefix::Milli )
+	/// );
+	/// assert_eq!(
+	/// 	SiNum::new( 1234.5 ).shorten().unwrap(),
+	/// 	SiNum::new( 1.2345 ).with_prefix( Prefix::Kilo )
+	/// );
+	/// ```
 	pub fn shorten( self ) -> Result<Self, PrefixError> {
 		let exps = self.mantissa.log10().floor().div_euclid( 3.0 ) * 3.0;
 
@@ -201,23 +278,55 @@ impl SiNum {
 		Ok( self.to_prefix( prefix_new ) )
 	}
 
-	/// Returns the mantissa of the `SiNum`.
+	/// Returns the mantissa of the `SiNum`. The Mantissa is the number displayed before the prefix.
+	///
+	/// # Example
+	/// ```
+	/// # use sinum::{SiNum, Prefix};
+	/// let num = SiNum::new( 9999.9 );
+	///
+	/// assert_eq!( num.mantissa(), 9999.9 );
+	/// assert_eq!( num.with_prefix( Prefix::Mega ).mantissa(), 9999.9 );
+	/// assert_eq!( num.with_prefix( Prefix::Milli ).mantissa(), 9999.9 );
+	/// ```
 	pub fn mantissa( &self ) -> f64 {
 		self.mantissa
 	}
 
-	/// Returns the prefix of the `SiNum`.
+	/// Returns the `Prefix` of the `SiNum`.
+	///
+	/// # Example
+	/// ```
+	/// # use sinum::{SiNum, Prefix};
+	/// let num = SiNum::new( 9999.9 ).with_prefix( Prefix::Mega );
+	/// assert_eq!( num.prefix(), Prefix::Mega );
+	/// ```
 	pub fn prefix( &self ) -> Prefix {
 		self.prefix
 	}
 
 	/// Returns the numeric value of the `SiNum` without any prefix.
+	///
+	/// # Example
+	/// ```
+	/// # use sinum::SiNum;
+	/// assert_eq!( SiNum::new( 9999.9 ).as_f64(), 9999.9 );
+	/// assert_eq!( SiNum::new( 99999.9 ).as_f64(), 99999.9 );
+	/// ```
 	pub fn as_f64( &self ) -> f64 {
 		self.mantissa * self.prefix.as_f64()
 	}
 }
 
 impl From<f64> for SiNum {
+	/// Creates a new `SiNum` from `item`. This is identical to `SiNum::new()`.
+	///
+	/// # Example
+	/// ```
+	/// # use sinum::SiNum;
+	/// assert_eq!( SiNum::from( 9999.9 ), SiNum::new( 9999.9 ) );
+	/// assert_eq!( SiNum::from( 99999.9 ), SiNum::new( 99999.9 ) );
+	/// ```
 	fn from( item: f64 ) -> Self {
 		Self {
 			mantissa: item,
@@ -247,79 +356,9 @@ mod tests {
 	use super::*;
 
 	#[test]
-	fn prefix_factor() {
-		assert_eq!( Prefix::Peta.as_f64(), 1e15f64 );
-		assert_eq!( Prefix::Femto.as_f64(), 1e-15f64 );
-	}
-
-	#[test]
-	fn prefix_exp() {
-		assert_eq!( Prefix::Peta.exp(), 15i8 );
-		assert_eq!( Prefix::Femto.exp(), -15i8 );
-	}
-
-	#[test]
-	fn from_integer() {
-		assert_eq!( Prefix::try_from( -3 ).unwrap(), Prefix::Milli );
-		assert_eq!( Prefix::try_from( -2 ).unwrap(), Prefix::Centi );
-		assert_eq!( Prefix::try_from( 0 ).unwrap(), Prefix::Nothing );
-		assert_eq!( Prefix::try_from( 3 ).unwrap(), Prefix::Kilo );
-		assert_eq!( Prefix::try_from( 15 ).unwrap(), Prefix::Peta );
-	}
-
-	#[test]
 	fn print_prefix() {
 		assert_eq!( Prefix::Peta.to_string(), "P".to_string() );
 		assert_eq!( Prefix::Femto.to_string(), "f".to_string() );
-	}
-
-	#[test]
-	fn create_sinum() {
-		assert_eq!( SiNum::from( 9999.9 ), SiNum::new( 9999.9 ) );
-		assert_eq!( SiNum::from( 99999.9 ), SiNum::new( 99999.9 ) );
-	}
-
-	#[test]
-	fn sinum_value() {
-		assert_eq!( SiNum::new( 9999.9 ).as_f64(), 9999.9f64 );
-		assert_eq!( SiNum::new( 99999.9 ).as_f64(), 99999.9f64 );
-	}
-
-	#[test]
-	fn sinum_with_prefix() {
-		// Adding a specific prefix is *modifying* the value by keeping the mantissa the same and switching the prefix.
-		assert_eq!( SiNum::new( 9999.9 ).with_prefix( Prefix::Mega ).as_f64(), 9999900000.0f64 );
-		assert_eq!( SiNum::new( 9999.9 ).with_prefix( Prefix::Milli ).as_f64(), 9.9999f64 );
-	}
-
-	#[test]
-	fn sinum_prefix() {
-		assert_eq!( SiNum::new( 9999.9 ).with_prefix( Prefix::Mega ).prefix(), Prefix::Mega );
-		assert_eq!( SiNum::new( 9999.9 ).with_prefix( Prefix::Milli ).prefix(), Prefix::Milli );
-	}
-
-	#[test]
-	fn sinum_mantissa() {
-		// The Mantissa is the number displayed before the prefix.
-		assert_eq!( SiNum::new( 9999.9 ).mantissa(), 9999.9 );
-		assert_eq!( SiNum::new( 9999.9 ).with_prefix( Prefix::Mega ).mantissa(), 9999.9 );
-		assert_eq!( SiNum::new( 9999.9 ).with_prefix( Prefix::Milli ).mantissa(), 9999.9 );
-	}
-
-	#[test]
-	fn sinum_to_prefix() {
-		// Converting a `SiNum` to a new prefix shall not change the value itself, only its representation.
-		assert_eq!( SiNum::new( 9999.9 ).with_prefix( Prefix::Mega ).as_f64(), 9999900000.0f64 );
-		assert_eq!( SiNum::new( 9999.9 ).with_prefix( Prefix::Mega ).to_prefix( Prefix::Milli ).as_f64(), 9999900000.0f64 );
-		assert_eq!( SiNum::new( 9999.9 ).with_prefix( Prefix::Mega ).to_prefix( Prefix::Milli ).mantissa(), 9999900000000.0f64 );
-	}
-
-	#[test]
-	fn sinum_shorten() {
-		// "shorten" means reducing the number of digits to represent the number.
-		assert_eq!( SiNum::new( 1000.0 ).shorten().unwrap(), SiNum::new( 1.0 ).with_prefix( Prefix::Kilo ) );
-		assert_eq!( SiNum::new( 0.001 ).shorten().unwrap(), SiNum::new( 1.0 ).with_prefix( Prefix::Milli ) );
-		assert_eq!( SiNum::new( 1234.5 ).shorten().unwrap(), SiNum::new( 1.2345 ).with_prefix( Prefix::Kilo ) );
 	}
 
 	#[test]
