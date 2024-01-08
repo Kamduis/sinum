d67bbf8740ea3c038863b6faaccb1b462e42da32
Victor <victor@caern.de>
2024-01-08 19:19:25 +0100

    Added more prefixes.

--- src/prefix.rs
+++ src/prefix.rs
@@ -39,6 +39,9 @@ pub enum PrefixError {
 /// Represents the different SI prefixes like kilo, milli, nano etc.
 #[derive( Clone, Copy, PartialOrd, Ord, PartialEq, Eq, Debug )]
 pub enum Prefix {
+	Yocto,
+	Zepto,
+	Atto,
 	Femto,
 	Pico,
 	Nano,
@@ -47,11 +50,16 @@ pub enum Prefix {
 	Centi,
 	Deci,
 	Nothing,
+	Deca,
+	Hecto,
 	Kilo,
 	Mega,
 	Giga,
 	Tera,
 	Peta,
+	Exa,
+	Zetta,
+	Yotta,
 }
 
 impl Prefix {
@@ -71,6 +79,9 @@ impl Prefix {
 	/// ```
 	pub fn as_f64( &self ) -> f64 {
 		match self {
+			Self::Yocto => 1e-24,
+			Self::Zepto => 1e-21,
+			Self::Atto =>  1e-18,
 			Self::Femto => 1e-15,
 			Self::Pico =>  1e-12,
 			Self::Nano =>  1e-9,
@@ -79,11 +90,16 @@ impl Prefix {
 			Self::Centi => 1e-2,
 			Self::Deci =>  1e-1,
 			Self::Nothing => 1.0,
+			Self::Deca =>  1e1,
+			Self::Hecto => 1e2,
 			Self::Kilo =>  1e3,
 			Self::Mega =>  1e6,
 			Self::Giga =>  1e9,
 			Self::Tera =>  1e12,
 			Self::Peta =>  1e15,
+			Self::Exa =>   1e18,
+			Self::Zetta => 1e21,
+			Self::Yotta => 1e24,
 		}
 	}
 
@@ -97,6 +113,9 @@ impl Prefix {
 	/// ```
 	pub fn exp( &self ) -> i8 {
 		match self {
+			Self::Yocto =>  -24,
+			Self::Zepto =>  -21,
+			Self::Atto =>   -18,
 			Self::Femto =>  -15,
 			Self::Pico =>   -12,
 			Self::Nano =>    -9,
@@ -105,11 +124,16 @@ impl Prefix {
 			Self::Centi =>   -2,
 			Self::Deci =>    -1,
 			Self::Nothing =>  0,
+			Self::Deca =>     1,
+			Self::Hecto =>    2,
 			Self::Kilo =>     3,
 			Self::Mega =>     6,
 			Self::Giga =>     9,
 			Self::Tera =>    12,
 			Self::Peta =>    15,
+			Self::Exa =>     18,
+			Self::Zetta =>   21,
+			Self::Yotta =>   24,
 		}
 	}
 }
@@ -132,6 +156,9 @@ impl TryFrom<i8> for Prefix {
 	/// ```
 	fn try_from( item: i8 ) -> Result<Self, Self::Error> {
 		let res = match item {
+			-24 => Self::Yocto,
+			-21 => Self::Zepto,
+			-18 => Self::Atto,
 			-15 => Self::Femto,
 			-12 => Self::Pico,
 			-9  => Self::Nano,
@@ -140,11 +167,16 @@ impl TryFrom<i8> for Prefix {
 			-2  => Self::Centi,
 			-1  => Self::Deci,
 			0   => Self::Nothing,
+			1   => Self::Deca,
+			2   => Self::Hecto,
 			3   => Self::Kilo,
 			6   => Self::Mega,
 			9   => Self::Giga,
 			12  => Self::Tera,
 			15  => Self::Peta,
+			18  => Self::Exa,
+			21  => Self::Zetta,
+			24  => Self::Yotta,
 			_ => return Err( PrefixError::TryFromExp( item ) ),
 		};
 
@@ -155,6 +187,9 @@ impl TryFrom<i8> for Prefix {
 impl fmt::Display for Prefix {
 	fn fmt( &self, f: &mut fmt::Formatter ) -> fmt::Result {
 		match self {
+			Self::Yocto =>   write!( f, "y" ),
+			Self::Zepto =>   write!( f, "z" ),
+			Self::Atto =>    write!( f, "a" ),
 			Self::Femto =>   write!( f, "f" ),
 			Self::Pico =>    write!( f, "p" ),
 			Self::Nano =>    write!( f, "n" ),
@@ -163,11 +198,16 @@ impl fmt::Display for Prefix {
 			Self::Centi =>   write!( f, "c" ),
 			Self::Deci =>    write!( f, "d" ),
 			Self::Nothing => write!( f, "" ),
+			Self::Deca =>    write!( f, "da" ),
+			Self::Hecto =>   write!( f, "h" ),
 			Self::Kilo =>    write!( f, "k" ),
 			Self::Mega =>    write!( f, "M" ),
 			Self::Giga =>    write!( f, "G" ),
 			Self::Tera =>    write!( f, "T" ),
 			Self::Peta =>    write!( f, "P" ),
+			Self::Exa =>     write!( f, "E" ),
+			Self::Zetta =>   write!( f, "Z" ),
+			Self::Yotta =>   write!( f, "Y" ),
 		}
 	}
 }
@@ -188,6 +228,9 @@ impl Latex for Prefix {
 	/// ```
 	fn to_latex( &self ) -> String {
 		match self {
+			Self::Yocto =>   format!( r"\yocto" ),
+			Self::Zepto =>   format!( r"\zepto" ),
+			Self::Atto =>    format!( r"\atto" ),
 			Self::Femto =>   format!( r"\femto" ),
 			Self::Pico =>    format!( r"\pico" ),
 			Self::Nano =>    format!( r"\nano" ),
@@ -196,11 +239,16 @@ impl Latex for Prefix {
 			Self::Centi =>   format!( r"\centi" ),
 			Self::Deci =>    format!( r"\deca" ),
 			Self::Nothing => format!( "" ),
+			Self::Deca =>    format!( r"\deca" ),
+			Self::Hecto =>   format!( r"\hecto" ),
 			Self::Kilo =>    format!( r"\kilo" ),
 			Self::Mega =>    format!( r"\mega" ),
 			Self::Giga =>    format!( r"\giga" ),
 			Self::Tera =>    format!( r"\tera" ),
 			Self::Peta =>    format!( r"\peta" ),
+			Self::Exa =>     format!( r"\exa" ),
+			Self::Zetta =>   format!( r"\zetta" ),
+			Self::Yotta =>   format!( r"\yotta" ),
 		}
 	}
 }
